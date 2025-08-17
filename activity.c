6f8ee1857a9a87737154e2be1b4659248bf218fd
sazoszy <kimdarwinculbata@gmail.com>
2025-08-17 21:07:57 +0800

    Add files via upload

---
+++ activity.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+
+/*
+int main() {
+    
+    char userName;
+    int passCode;
+    
+
+        freshstart:
+        printf("Enter Username: ");
+        scanf("%c", &userName);
+        
+        if (userName == 'F') {
+            goto passWord;
+        } else if (userName == 'G') {
+            goto passWord;
+        } else if (userName == 'H') {
+            goto passWord;
+        } else {
+            printf("Username is invalid. Please try again.\n"); goto freshstart;
+        }
+
+        passWord:
+        printf("Enter Password: ");
+        scanf("%d", &passCode);
+
+        if (userName == 'F' && passCode == 213) {
+            printf("Login Successful. Welcome Admin A.\n");
+        } else if (userName == 'G' && passCode == 214) {
+            printf("Login Successful. Welcome Admin B.\n");
+        } else if (userName == 'H' && passCode == 215) {
+            printf("Login Successful. Welcome Admin C.\n");
+        } else {
+            printf("Incorrect Passcode. Please try again.\n"); goto passWord;
+        }
+
+    
+
+*/
+
+/*
+int main() {
+
+    char opers;
+    double firstNum;
+    double secondNum;
+    double result;
+
+        printf("Enter an operator (+ - / *): ");
+        scanf("%c", &opers);
+
+        printf("Enter First Number: ");
+        scanf("%lf", &firstNum);
+        
+        printf("Enter Second Number: ");
+        scanf("%lf", &secondNum);
+
+        switch(opers) {
+            case '+':
+                result = firstNum + secondNum;
+                    printf("result: %.2lf", result);
+                break;
+            case '-':
+                result = firstNum - secondNum;
+                    printf("result: %.2lf", result);
+                break;
+            case '/':
+                result = firstNum / secondNum;
+                    printf("result: %.2lf", result);
+                break;
+            case '*':
+                result = firstNum * secondNum;
+                    printf("result: %.2lf", result);
+                break;
+            default:
+                printf("%c is not a valid operator.", opers);
+            }
+ */       
+    return 0;
+}
